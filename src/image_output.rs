1e26311b446a96dc08de772b7a7103e99ae820ea
Robert Brignull <robertbrignull@gmail.com>
2019-04-16 22:08:00 +0100

    output flake as png image

---
+++ src/image_output.rs
@@ -0,0 +1,45 @@
+use std::f64::INFINITY;
+use std::path::Path;
+
+use crate::data::Point;
+
+const BORDER: f64 = 10.0;
+
+pub fn save_png(points: Vec<Point>) {
+    let mut top: f64 = INFINITY;
+    let mut right: f64 = -INFINITY;
+    let mut bottom: f64 = -INFINITY;
+    let mut left: f64 = INFINITY;
+
+    for point in &points {
+        top = top.min(point.y);
+        right = right.max(point.x);
+        bottom = bottom.max(point.y);
+        left = left.min(point.x);
+    }
+
+    let width = (right - left + BORDER * 2.0 + 1.0).ceil() as usize;
+    let height = (bottom - top + BORDER * 2.0 + 1.0).ceil() as usize;
+    let left = left - BORDER;
+    let top = top - BORDER;
+
+    let mut buffer: Vec<u8> = vec![0; width * height * 3];
+
+    for point in &points {
+        let x = (point.x - left) as usize;
+        let y = (point.y - top) as usize;
+        let i = (x + y * width) * 3;
+        buffer[i] = 255;
+        buffer[i + 1] = 255;
+        buffer[i + 2] = 255;
+    }
+
+    // Save the buffer as "image.png"
+    image::save_buffer(
+        &Path::new("output.png"),
+        &buffer,
+        width as u32,
+        height as u32,
+        image::RGB(8))
+        .expect("Unable to save image");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
+extern crate image;
 extern crate rand;
 
 mod data;
 mod grid;
+mod image_output;
 mod square_grid;
 
 use crate::grid::Grid;
+use crate::image_output::save_png;
 use crate::square_grid::SquareGrid;
 
 fn main() {
     let mut grid: Box<Grid> = Box::new(SquareGrid::new());
 
-    grid.add_points(500);
+    grid.add_points(10000);
 
-    for p in grid.list_points() {
-        println!("{}", p);
-    }
+    save_png(grid.list_points());
 }
