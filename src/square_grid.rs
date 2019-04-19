24b9da2acd7bde5c49f920e47d971982ebff68ea
Robert Brignull <robertbrignull@gmail.com>
2019-04-19 15:55:27 +0100

    use an expanding ciruclar border

--- src/square_grid.rs
+++ src/square_grid.rs
@@ -1,33 +1,36 @@
 extern crate rand;
 
 use rand::Rng;
+use std::f64::consts::PI;
 
 use crate::data::Point;
 use crate::grid::Grid;
 
+const BORDER: f64 = 20.0;
+
 pub struct SquareGrid {
     grid: Vec<Vec<bool>>,
+    radius: f64,
 }
 
 impl SquareGrid {
     pub fn new() -> Self {
-        let w = 40;
+        let w = BORDER as usize * 2 + 100;
         let mut grid = vec![vec![false; w]; w];
         grid[w / 2][w / 2] = true;
 
         return SquareGrid {
             grid,
+            radius: BORDER,
         };
     }
 
-    /** Is the given point nearly at the edge of the grid */
-    fn should_increase_grid_size(&self, p: Point) -> bool {
-        let lb = self.grid.len() as f64 / 6.0;
-        let ub = self.grid.len() as f64 - lb;
-        return p.x < lb ||
-            p.x > ub ||
-            p.y < lb ||
-            p.y > ub;
+    /** The distance from the given point to the centre of the grid */
+    fn distance_to_centre(&self, point: &Point) -> f64 {
+        let c = (self.grid.len() / 2) as f64;
+        let d = (point.x - c) * (point.x - c) +
+            (point.y - c) * (point.y - c);
+        return d.sqrt();
     }
 
     /** Double the grid size */
@@ -57,57 +60,31 @@ impl SquareGrid {
 
     /** Get a random position of the border of the grid */
     fn get_start_position(&self) -> Point {
-        let mut rng = rand::thread_rng();
-        let dir = rng.gen::<u32>() % 4;
-        return match dir {
-            // up
-            0 => Point {
-                x: rng.gen::<usize>().min(self.grid.len() - 1) as f64,
-                y: 0.0,
-            },
-            // right
-            1 => Point {
-                x: (self.grid.len() - 1) as f64,
-                y: rng.gen::<usize>().min(self.grid.len() - 1) as f64,
-            },
-            // down
-            2 => Point {
-                x: rng.gen::<usize>().min(self.grid.len() - 1) as f64,
-                y: (self.grid.len() - 1) as f64,
-            },
-            // left
-            _ => Point {
-                x: 0.0,
-                y: rng.gen::<usize>().min(self.grid.len() - 1) as f64,
-            },
+        let dir = rand::thread_rng().gen::<f64>() * PI * 2.0;
+        // Round towards zero to keep inside the radius
+        let dx = (self.radius * dir.sin()).trunc();
+        let dy = (self.radius * dir.cos()).trunc();
+
+        return Point {
+            x: self.grid.len() as f64 / 2.0 + dx,
+            y: self.grid.len() as f64 / 2.0 + dy,
         };
     }
 
     /** Walk the particle one step */
-    fn get_next_position(&self, point: &Point) -> Point {
+    fn get_next_position(&self, point: Point) -> Point {
         let dir = rand::thread_rng().gen::<u32>() % 4;
-        return match dir {
-            // up
-            0 => Point {
-                x: point.x,
-                y: (point.y - 1.0).max(0.0),
-            },
-            // right
-            1 => Point {
-                x: (point.x + 1.0).min((self.grid.len() - 1) as f64),
-                y: point.y,
-            },
-            // down
-            2 => Point {
-                x: point.x,
-                y: (point.y + 1.0).min((self.grid.len() - 1) as f64),
-            },
-            // left
-            _ => Point {
-                x: (point.x - 1.0).max(0.0),
-                y: point.y,
-            },
+        let new_point = match dir {
+            0 => Point { x: point.x,       y: point.y - 1.0 },
+            1 => Point { x: point.x + 1.0, y: point.y       },
+            2 => Point { x: point.x,       y: point.y + 1.0 },
+            _ => Point { x: point.x - 1.0, y: point.y       },
         };
+        if self.distance_to_centre(&new_point) < self.radius {
+            return new_point;
+        } else {
+            return point;
+        }
     }
 
     /** Is the given point in contact with the flake */
@@ -117,10 +94,12 @@ impl SquareGrid {
         // It shouldn't happen but handle the case where it
         // is already overlapping with the flake.
         return self.grid[x][y] ||
-            (y > 0 && self.grid[x][y - 1]) ||
-            (x < self.grid.len() - 1 && self.grid[x + 1][y]) ||
-            (y < self.grid.len() - 1 && self.grid[x][y + 1]) ||
-            (x > 0 && self.grid[x - 1][y]);
+            // Because of the boundary circle we don't need
+            // to worry about going off the edge of the grid.
+            self.grid[x][y - 1] ||
+            self.grid[x + 1][y] ||
+            self.grid[x][y + 1] ||
+            self.grid[x - 1][y];
     }
 }
 
@@ -128,11 +107,13 @@ impl Grid for SquareGrid {
     fn add_point(&mut self) {
         let mut point = self.get_start_position();
         while !self.has_hit_flake(&point) {
-            point = self.get_next_position(&point);
+            point = self.get_next_position(point);
         }
         self.grid[point.x as usize][point.y as usize] = true;
+        self.radius = self.radius.max(self.distance_to_centre(&point) + 10.0);
 
-        if self.should_increase_grid_size(point) {
+        // If we're even getting close to the grid then increase the size
+        if self.radius >= self.grid.len() as f64 / 2.0 - 10.0 {
             self.increase_grid_size();
         }
     }
