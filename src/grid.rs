94340738abe363d1b57b303f56aedc468976077e
Robert Brignull <robertbrignull@gmail.com>
2019-04-19 09:30:39 +0100

    Output number of points at regular intervals

--- src/grid.rs
+++ src/grid.rs
@@ -1,9 +1,18 @@
+use std::time::Instant;
+
 use crate::data::Point;
 
 pub trait Grid {
     fn add_points(&mut self, num_points: i32) {
-        for _i in 0..num_points {
+        let mut t = Instant::now();
+        for i in 0..num_points {
             self.add_point();
+
+            let now = Instant::now();
+            if now.duration_since(t).as_millis() > 500 {
+                t = now;
+                println!("Added {} points", i);
+            }
         }
     }
 
