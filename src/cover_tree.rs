e35b0929022398b42984051b535c1d231cdcc818
Robert Brignull <robertbrignull@gmail.com>
2021-10-03 16:31:14 +0100

    add tests of cover tree

--- src/cover_tree.rs
+++ src/cover_tree.rs
@@ -42,10 +42,70 @@ impl CoverTree {
                 min_distance_2 = d;
             }
         }
-        return nearest_point;
+        return (nearest_point, min_distance_2.sqrt());
     }
 
     pub fn get_farthest_distance(&self) -> f64 {
         return self.farthest_distance;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::CoverTree;
+    use crate::flake::Flake;
+    use crate::point::Point;
+    use crate::test_utils::test::with_test_dir;
+
+    #[test]
+    fn is_empty() {
+        with_test_dir(|test_dir: &str| {
+            let flake = Flake::new(&format!("{}/test.flake", test_dir));
+            let mut tree = CoverTree::from_flake(&flake).expect("Unable to make flake");
+            assert_eq!(true, tree.is_empty());
+
+            tree.add_point(Point::ZERO);
+            assert_eq!(false, tree.is_empty());
+        });
+    }
+
+    #[test]
+    fn get_nearest() {
+        with_test_dir(|test_dir: &str| {
+            let flake = Flake::new(&format!("{}/test.flake", test_dir));
+            let mut tree = CoverTree::from_flake(&flake).expect("Unable to make flake");
+
+            tree.add_point(Point::ZERO);
+            assert_eq!(0.0, tree.get_nearest(Point::ZERO).1);
+            assert_eq!(1.0, tree.get_nearest(Point { x: 1.0, y: 0.0 }).1);
+            assert_eq!(4.0, tree.get_nearest(Point { x: 0.0, y: 4.0 }).1);
+
+            tree.add_point(Point { x: 0.0, y: 1.0 });
+            assert_eq!(0.0, tree.get_nearest(Point::ZERO).1);
+            assert_eq!(5.0, tree.get_nearest(Point { x: 5.0, y: 1.0 }).1);
+            assert_eq!(6.0, tree.get_nearest(Point { x: 6.0, y: 0.0 }).1);
+
+            tree.add_point(Point { x: 10.0, y: 0.0 });
+            assert_eq!(0.0, tree.get_nearest(Point::ZERO).1);
+            assert_eq!(5.0, tree.get_nearest(Point { x: 5.0, y: 1.0 }).1);
+            assert_eq!(4.0, tree.get_nearest(Point { x: 6.0, y: 0.0 }).1);
+        });
+    }
+
+    #[test]
+    fn get_farthest_distance() {
+        with_test_dir(|test_dir: &str| {
+            let flake = Flake::new(&format!("{}/test.flake", test_dir));
+            let mut tree = CoverTree::from_flake(&flake).expect("Unable to make flake");
+
+            tree.add_point(Point::ZERO);
+            assert_eq!(0.0, tree.get_farthest_distance());
+
+            tree.add_point(Point { x: 0.0, y: 1.0 });
+            assert_eq!(1.0, tree.get_farthest_distance());
+
+            tree.add_point(Point { x: 10.0, y: 0.0 });
+            assert_eq!(10.0, tree.get_farthest_distance());
+        });
     }
 }
