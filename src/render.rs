c84b394e6231ed212ce19dd20079dc92326d45c2
Robert Brignull <robertbrignull@gmail.com>
2024-08-15 14:26:46 +0100

    Extract methods for drawing

--- src/render.rs
+++ src/render.rs
@@ -3,47 +3,64 @@ use std::path::Path;
 
 use anyhow::{Context, Result};
 
-use crate::flake::Flake;
+use crate::{flake::Flake, point::Point};
 
 const BORDER: f64 = 10.0;
+
+struct Window {
+    left: f64,
+    top: f64,
+    width: usize,
+    height: usize,
+}
+
+fn find_bounding_rect(points: Vec<Point>) -> Window {
+    let mut left: f64 = INFINITY;
+    let mut top: f64 = INFINITY;
+    let mut right: f64 = -INFINITY;
+    let mut bottom: f64 = -INFINITY;
+
+    for point in &points {
+        left = left.min(point.x);
+        top = top.min(point.y);
+        right = right.max(point.x);
+        bottom = bottom.max(point.y);
+    }
+    
+    let left = left - BORDER;
+    let top = top - BORDER;
+    let width = (right - left + BORDER + 1.0).ceil() as usize;
+    let height = (bottom - top + BORDER + 1.0).ceil() as usize;
+
+    return Window { left, top, width, height };
+}
+
+fn draw_point(point: &Point, window: &Window, buffer: &mut Vec<u8>) {
+    let x = (point.x - window.left) as usize;
+    let y = (point.y - window.top) as usize;
+    let i = (x + y * window.width) * 3;
+    buffer[i] = 255;
+    buffer[i + 1] = 255;
+    buffer[i + 2] = 255;
+}
 
 pub fn render(flake: &Flake, output_filename: &str) -> Result<()> {
     let points = flake.get_points().context("Unable to get points for flake")?;
 
-    let mut top: f64 = INFINITY;
-    let mut right: f64 = -INFINITY;
-    let mut bottom: f64 = -INFINITY;
-    let mut left: f64 = INFINITY;
+    let window = find_bounding_rect(points.clone());
+
+    let mut buffer: Vec<u8> = vec![0; window.width * window.height * 3];
 
     for point in &points {
-        top = top.min(point.y);
-        right = right.max(point.x);
-        bottom = bottom.max(point.y);
-        left = left.min(point.x);
-    }
-
-    let width = (right - left + BORDER * 2.0 + 1.0).ceil() as usize;
-    let height = (bottom - top + BORDER * 2.0 + 1.0).ceil() as usize;
-    let left = left - BORDER;
-    let top = top - BORDER;
-
-    let mut buffer: Vec<u8> = vec![0; width * height * 3];
-
-    for point in &points {
-        let x = (point.x - left) as usize;
-        let y = (point.y - top) as usize;
-        let i = (x + y * width) * 3;
-        buffer[i] = 255;
-        buffer[i + 1] = 255;
-        buffer[i + 2] = 255;
+        draw_point(point, &window, &mut buffer)
     }
 
     // Save the buffer as "image.png"
     image::save_buffer(
         &Path::new(output_filename),
         &buffer,
-        width as u32,
-        height as u32,
+        window.width as u32,
+        window.height as u32,
         image::ColorType::Rgb8,
     )
     .context("Unable to save image buffer to file")?;
